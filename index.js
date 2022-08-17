0476b733276d3a0c204772dd0a777f49fec24e0b
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-17 15:58:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1, num2) {
+  num1 = 2000;
+  num2 = 100;
+  return num1 / num2;
+}
+
+const square = (x) => x * x;
+square(2, 2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3, 4);
